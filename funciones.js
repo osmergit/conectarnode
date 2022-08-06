41a41e3dffcdbd145b7fc34bd25e07f5628f2d0f
OSMER EDUARDO RUIZ MATEUS <osmer1976@gmail.com>
2022-08-06 18:35:25 -0500

    archivos
    ejemplos

---
+++ funciones.js
@@ -0,0 +1,18 @@
+function add(x,y) {
+    return x + y;
+}
+
+function resta(x,y){
+    return x - y;
+}
+
+function multiplicar(x,y){
+    return x * y;
+}
+function dividir(x,y){
+    if (y == 0) {
+        console.log('No se puede dividir por cero');
+    } else {
+        return x/y;
+    }
+}
---
+++ index.js
@@ -0,0 +1,20 @@
+require('./funciones.js')
+const http = require('http');
+
+const hostname = '44.210.24.228';
+const port = 80;
+const iniciarservidor = function (req,res)
+{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1>Hola Mundo</h1>');
+    res.end();
+
+}
+
+const server = http.createServer(iniciarservidor);
+
+server.listen(port, hostname, function ()  {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+console.log(add(5,10));
