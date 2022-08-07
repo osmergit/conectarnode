00c89ea681c2410b0893e4fcf51eebcdbfb3d452
OSMER EDUARDO RUIZ MATEUS <osmer1976@gmail.com>
2022-08-06 21:27:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const port = 3000;
+const port = 80;
 const server = http.createServer((req,res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
