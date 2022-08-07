cc4bef49f114b2ec771c8d3fa751aa90da7825a7
OSMER EDUARDO RUIZ MATEUS <osmer1976@gmail.com>
2022-08-06 19:39:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require('./funciones.js')
 const http = require('http');
 
-const hostname = '44.210.24.228';
-const port = 80;
-const iniciarservidor = function (req,res)
+//const hostname = '44.210.24.228';
+const port = 3000;
+const iniciarservidor =  function (req,res)
 {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
@@ -14,7 +14,7 @@ const iniciarservidor = function (req,res)
 
 const server = http.createServer(iniciarservidor);
 
-server.listen(port, hostname, function ()  {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running at http://${port}/`);
 });
 console.log(add(5,10));
