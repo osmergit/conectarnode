97924d7a31056537ef692e5dd752c7c1fc1c74d8
OSMER EDUARDO RUIZ MATEUS <osmer1976@gmail.com>
2022-08-06 21:17:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-
 const http = require('http');
 
-const hostname = 'ip-172-31-81-240.ec2.internal';
-const port = 80;
-const iniciarservidor =  function (req,res)
-{
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h1>Hola Mundo</h1>');
-    res.end();
+//const hostname = 'ip-172-31-81-240.ec2.internal';
+const port = 3000;
 
-}
 
-const server = http.createServer(iniciarservidor);
+const server = http.createServer((req,res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<h1>Hola Mundo</h1>');
+  res.end();
 
-server.listen(port, hostname, function () {
-  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://${port}/`);
 });
 
